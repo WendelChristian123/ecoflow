46ebd1db4b3f22ad2d0bf6f3b7d8eab7228ae22f
WendelChristian123 <wendel.d.f93@gmail.com>
2026-06-05 18:10:55 -0400

    chore(pwa): update dev service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.90r67fja834"
+    "revision": "0.ato9gc4m04g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
